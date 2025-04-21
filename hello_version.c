e819e9b42dc4ed2dcd1c4be448d0af7afa6b48b5
Martin Cvitic <martincvitic19@gmail.com>
2025-04-21 06:09:12 +0200

    feat(hello_version): update source to include module uptime

--- hello_version.c
+++ hello_version.c
@@ -1,20 +1,33 @@
-
 // SPDX-License-Identifier: GPL-2.0
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/utsname.h>  // for utsname()
+#include <linux/utsname.h>
+#include <linux/timekeeping.h>
+
+static char *who = "World";
+module_param(who, charp, 0000);
+MODULE_PARM_DESC(who, "Who to greet");
+
+static unsigned long load_time;
 
 static int __init hello_init(void)
 {
-    struct new_utsname *u = utsname();
-    printk(KERN_INFO "Hello World. You are currently using Linux %s.\n", u->release);
-    return 0;
+	struct new_utsname *u = utsname();
+
+	load_time = ktime_get_seconds();
+
+	pr_info("Hello %s. You are currently using Linux %s.\n", who, u->release);
+
+	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    printk(KERN_INFO "Goodbye! Unloading the hello_version module.\n");
+	unsigned long unload_time = ktime_get_seconds();
+	unsigned long elapsed = unload_time - load_time;
+
+	pr_info("Goodbye %s! Module was loaded for %lu seconds.\n", who, elapsed);
 }
 
 module_init(hello_init);
@@ -22,5 +35,6 @@ module_exit(hello_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Martin Cvitic");
-MODULE_DESCRIPTION("Kernel module to display runtime Linux version info");
-MODULE_VERSION("1.0");
+MODULE_DESCRIPTION("Kernel module to greet and show version and uptime");
+MODULE_VERSION("1.1");
+
